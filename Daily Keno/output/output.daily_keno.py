dae28db403ea37151fd5015385b118352b989629
AndrewGoodCode <143739789+AndrewGoodCode@users.noreply.github.com>
2023-09-08 18:05:53 -0400

    Update

--- Daily Keno/output/output.daily_keno.py
+++ Daily Keno/output/output.daily_keno.py
@@ -7,7 +7,7 @@ print("Current Working Directory:", os.getcwd())
 
 # Define the file paths
 analysis_file_path = os.path.join(os.getcwd(), 'data.daily_keno.json')
-daily_keno_numbers_file_path = os.path.join(os.getcwd(), 'daily_keno_numbers.json')
+daily_keno_numbers_file_path = os.path.join(os.path.dirname(os.getcwd()), 'daily_keno_numbers.json')
 output_file_path = os.path.join(os.getcwd(), 'output.daily_keno.json')
 
 # Check if the analysis file exists
